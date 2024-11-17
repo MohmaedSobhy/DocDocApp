045175c234317b0f33d0ba5918cb3199995b2a5d
Mohamed sobhy <mohamedyamany@055@gmail.com>
2024-11-17 17:03:46 +0200

    app string

---
+++ lib/core/utils/app_string.dart
@@ -0,0 +1,7 @@
+abstract class AppString {
+  static const String appName = 'DocDoc';
+  static const String bestDector = 'Best Doctor\nAppointment App';
+  static const String getStarted = 'Get Started';
+  static const String boardingDescription =
+      'Manage and schedule all of your medical appointments easily with Docdoc to get a new experien';
+}
